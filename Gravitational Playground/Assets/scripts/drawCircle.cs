6b91192d127324035b5b54feafd735cdfa6335b6
NoahWantsDie <memelord09865@gmail.com>
2022-10-20 00:29:36 -0500

    updates
    simple planet in orbit tool and time scale bar as well as other small tweaks

---
+++ Gravitational Playground/Assets/scripts/drawCircle.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawCircle : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DrawPolygon(int vertexNumber, float radius, Vector3 centerPos, float startWidth, float endWidth)
+    {
+        lineRenderer.startWidth = startWidth;
+        lineRenderer.endWidth = endWidth;
+        lineRenderer.loop = true;
+        float angle = 2 * Mathf.PI / vertexNumber;
+        lineRenderer.positionCount = vertexNumber;
+
+        for (int i = 0; i < vertexNumber; i++)
+        {
+            Matrix4x4 rotationMatrix = new Matrix4x4(new Vector4(Mathf.Cos(angle * i), Mathf.Sin(angle * i), 0, 0),
+                                                     new Vector4(-1 * Mathf.Sin(angle * i), Mathf.Cos(angle * i), 0, 0),
+                                       new Vector4(0, 0, 1, 0),
+                                       new Vector4(0, 0, 0, 1));
+            Vector3 initialRelativePosition = new Vector3(0, radius, 0);
+            lineRenderer.SetPosition(i, centerPos + rotationMatrix.MultiplyPoint(initialRelativePosition));
+
+        }
+    }
+}
--- Gravitational Playground/Assets/scripts/main.cs
+++ Gravitational Playground/Assets/scripts/main.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class main : MonoBehaviour
 {
+
+    public float TimeStep = 1;
     public GameObject sun;
     public GameObject particle;
 
@@ -12,6 +14,7 @@ public class main : MonoBehaviour
     public Vector2 mousePosWorld;
 
     private Camera cam;
+    private float fixedDeltaTime;
 
     public GameObject focusObj;
     public GameObject explosion;
@@ -19,6 +22,8 @@ public class main : MonoBehaviour
     public Vector2 mouseDownPoint;
     public Vector2 slingShotPoint;
     public CameraMove CamScript;
+    public drawCircle circleScript;
+    public drawCircle circleScript2;
 
     public LineRenderer line;
     private Vector3[] points;
@@ -27,72 +32,243 @@ public class main : MonoBehaviour
     public bool usingSlider;
 
     public Text massTxt;
-    
+    public Text timeTxt;
+
     public float newMass = 0.5f;
 
     public List<GameObject> bodies = new List<GameObject>();
     public List<GameObject> particles = new List<GameObject>();
 
+    public bool linin;
+
+    //tool states
+    public string ToolState;
+
+    bool p;
+
+    public Toggle togPlanet;
+
+    bool f;
+
+    GameObject bOne;
+    public GameObject CircleObj;
+    public GameObject CircleObj2;
+    public Toggle togFocus;
+    public Toggle togOrb;
+    public GameObject MassCenterObj;
+    public void PlanetPlace()
+    {
+
+
+        ToolState = "PlanetPlacer";
+    }
+    public void FocusPlace()
+    {
+
+        ToolState = "FocusPlacer";
+
+    }
+    public void OrbitPlace()
+    {
+
+        ToolState = "OrbitPlacer";
+
+    }
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
         line.enabled = false;
         CamScript = Camera.main.GetComponent<CameraMove>();
+        circleScript = CircleObj.GetComponent<drawCircle>();
+        circleScript2 = CircleObj2.GetComponent<drawCircle>();
         bodies.Add(sun);
         particles.Add(particle);
-        
+        MassCenterObj.GetComponent<LineRenderer>().enabled = false;
+    }
+    void Awake()
+    {
+        this.fixedDeltaTime = Time.fixedDeltaTime;
     }
     float WeightedAverage(float num1, float weight1, float num2, float weight2)
     {
         var averag = ((num1 * weight1) + (num2 * weight2)) / (weight1 + weight2);
         return (averag);
     }
+
+    void SetFocusObject(GameObject fo)
+    {
+        Debug.Log("set focus to : " + fo);
+        focusObj = fo;
+    }
     // Update is called once per frame
     void Update()
     {
+        Time.timeScale = TimeStep;
+        Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+        if (linin && (ToolState == "OrbitPlacer"))
+        {
+            Vector2 direction = new Vector2(bOne.transform.position.x, bOne.transform.position.y) - mousePosWorld;
+            Body orbitTargetScript1 = bOne.GetComponent<Body>();
+            mousePosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float radius = Vector3.Distance(bOne.transform.position, mousePosWorld);
+            circleScript.DrawPolygon(30, radius, bOne.transform.position, 0.1f, 0.1f);
+            circleScript2.DrawPolygon(30, Mathf.Sqrt(newMass / Mathf.PI)/2, mousePosWorld, 0.1f, 0.1f);
+            line.SetPosition(0, bOne.transform.position);
+            line.SetPosition(1, mousePosWorld);
+            MassCenterObj.transform.position = GetMassCenter(bOne.transform.position, mousePosWorld, orbitTargetScript1.mass,newMass);
+            line.enabled = true;
+            if (usingSlider)
+            {
+                circleScript.lineRenderer.enabled = false;
+                circleScript2.lineRenderer.enabled = false;
+            }
+            else
+            {
+                circleScript.lineRenderer.enabled = true;
+                circleScript2.lineRenderer.enabled = true;
+            }
+            MassCenterObj.GetComponent<LineRenderer>().enabled = true;
+            Vector2 diff = direction.normalized;
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            MassCenterObj.transform.eulerAngles = new Vector3(0f, 0f, rot_z - 90);
+        }
+        if (Input.GetMouseButtonUp(0) && linin && (ToolState == "OrbitPlacer"))
+        {
+            Vector2 direction = new Vector2(bOne.transform.position.x, bOne.transform.position.y) - mousePosWorld;
+            Body orbitTargetScript = bOne.GetComponent<Body>();
+            float radius = Vector3.Distance(bOne.transform.position, mousePosWorld);
+            
+            float OrbitSpeed = CalculateVel(orbitTargetScript.G, orbitTargetScript.mass, radius);
+            //float OrbitSpeedP = CalculateVel(orbitTargetScript.G, newMass, radius);
+            //float OrbitSpeedOrbiter = ((OrbitSpeed)* CircularOrbitThing(orbitTargetScript.mass, newMass));
+            //float OrbitSpeedParent = ((OrbitSpeedP)* CircularOrbitThing(newMass, orbitTargetScript.mass));
+
+            
+            Vector2 NewOrbitVelOrbiter = new Vector2(direction.y, -direction.x).normalized * OrbitSpeed;
+            //Vector2 NewOrbitVelParent = new Vector2(direction.y, -direction.x).normalized * OrbitSpeedParent;
+            SpawnNewBody(mousePosWorld.x, mousePosWorld.y, NewOrbitVelOrbiter.x + orbitTargetScript.rb.velocity.x, NewOrbitVelOrbiter.y + orbitTargetScript.rb.velocity.y, newMass, false);
+
+            
+            //orbitTargetScript.rb.velocity -= NewOrbitVelParent;
+            line.SetPosition(0, new Vector3(0, 0, 0));
+            line.SetPosition(1, new Vector3(0, 0, 0));
+            line.enabled = false;
+            linin = false;
+            MassCenterObj.GetComponent<LineRenderer>().enabled = false;
+        }
+        if(ToolState != "OrbitPlacer")
+        {
+            linin = false;
+        }
+        else
+        {
+            if (!linin)
+            {
+                circleScript2.lineRenderer.enabled = false;
+            }
+        }
+        if (!linin)
+        {
+            
+            circleScript.lineRenderer.enabled = false;
+            
+        }
+        if (ToolState == "PlanetPlacer")
+        {
+            togPlanet.isOn = true;
+        }
+        else
+        {
+            togPlanet.isOn = false;
+        }
+        if (ToolState == "OrbitPlacer")
+        {
+            togOrb.isOn = true;
+        }
+        else
+        {
+            togOrb.isOn = false;
+        }
+
+        if (ToolState == "FocusPlacer")
+        {
+            togFocus.isOn = true;
+        }
+        else
+        {
+            togFocus.isOn = false;
+        }
+
+        if (focusObj == null)
+        {
+            CamScript.focused = false;
+        }
         massTxt.text = "New Mass : " + newMass.ToString();
+        timeTxt.text = "TimeScale : " + TimeStep.ToString();
         CamScript.focusedObject = focusObj;
         mousePos = Input.mousePosition;
         mousePosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         dragClickForce = mouseDownPoint - mousePosWorld;
         slingShotPoint = (mouseDownPoint - mousePosWorld) + mouseDownPoint;
         
-        line.SetPosition(0, mouseDownPoint);
-        line.SetPosition(1, slingShotPoint);
-
-        if (Input.GetMouseButtonDown(0))
+        
+        if (ToolState == "PlanetPlacer")
         {
-            if (!usingSlider)
+            line.SetPosition(0, mouseDownPoint);
+            line.SetPosition(1, slingShotPoint);
+            if (Input.GetMouseButtonDown(0))
             {
-                mouseDownPoint = mousePosWorld;
-                mousePosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                slingShotPoint = (mouseDownPoint - mousePosWorld) + mouseDownPoint;
+                if (!usingSlider)
+                {
+                    mouseDownPoint = mousePosWorld;
+                    mousePosWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    slingShotPoint = (mouseDownPoint - mousePosWorld) + mouseDownPoint;
 
-                line.SetPosition(0, mouseDownPoint);
-                line.SetPosition(1, slingShotPoint);
+                    line.SetPosition(0, mouseDownPoint);
+                    line.SetPosition(1, slingShotPoint);
+                    circleScript2.DrawPolygon(30, Mathf.Sqrt(newMass / Mathf.PI) / 2, mousePosWorld, 0.1f, 0.1f);
+                    circleScript2.lineRenderer.enabled = true;
+                    line.enabled = true;
+                }
 
-                line.enabled = true;
+                //SpawnNewBody(mousePosWorld.x, mousePosWorld.y, 0, 0, 10);
+                //Debug.Log("clicked");
             }
-
-            //SpawnNewBody(mousePosWorld.x, mousePosWorld.y, 0, 0, 10);
-            //Debug.Log("clicked");
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (!usingSlider && line.enabled)
+            if (Input.GetMouseButtonUp(0))
             {
-                SpawnNewBody(mouseDownPoint.x, mouseDownPoint.y, dragClickForce.x, dragClickForce.y, newMass, false);
-                Debug.Log("clicked");
-                line.enabled = false;
-                line.SetPosition(0, new Vector3(0, 0, 0));
-                line.SetPosition(1, new Vector3(0, 0, 0));
+                if (!usingSlider && line.enabled)
+                {
+                    SpawnNewBody(mouseDownPoint.x, mouseDownPoint.y, dragClickForce.x, dragClickForce.y, newMass, false);
+                    Debug.Log("clicked");
+                    line.enabled = false;
+                    circleScript2.lineRenderer.enabled = false;
+                    line.SetPosition(0, new Vector3(0, 0, 0));
+                    line.SetPosition(1, new Vector3(0, 0, 0));
+                }
+
             }
-            
         }
         
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            FocusPlace();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            PlanetPlace();
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            newMass = newMass / 2;
+        }
+
         if (usingSlider)
+        {
             line.enabled = false;
+            circleScript.lineRenderer.enabled = false;
+        }
+            
 
         for (int i = 0; i < bodies.Count; i++)
         {
@@ -126,15 +302,19 @@ public class main : MonoBehaviour
                                 b1Script.transform.position = new Vector2(NewPointX, NewPointY);
                                 b1Script.rb.velocity = new Vector2(NewVelX, NewVelY);
                                 b1Script.mass = b1Script.mass + b2Script.mass;
+                                
                                 Destroy(b2);
                                 bodies.Remove(b2);
+                                
                             }
                             else
                             {
-
+                                b2Script.transform.position = new Vector2(NewPointX, NewPointY);
+                                b2Script.transform.position = new Vector2(NewPointX, NewPointY);
 
                                 b2Script.rb.velocity = new Vector2(NewVelX, NewVelY);
                                 b2Script.mass = b1Script.mass + b2Script.mass;
+                                
                                 Destroy(b1);
                                 bodies.Remove(b1);
                             }
@@ -156,6 +336,32 @@ public class main : MonoBehaviour
                                 bodies.Remove(b1);
                             }
                         }
+                        if (ToolState == "FocusPlacer")
+                        {
+                            if ((distanceToMouse <= b1Script.radius) && (Input.GetMouseButtonDown(0)))
+                            {
+                                if (b1 != bodies[0])
+                                {
+
+                                    SetFocusObject(b1);
+                                    CamScript.focused = true;
+                                }
+                            }
+                        }
+                        if (ToolState == "OrbitPlacer")
+                        {
+                            
+                            if ((distanceToMouse <= b1Script.radius) && (Input.GetMouseButtonDown(0)))
+                            {
+                                linin = true;
+                                if (b1 != bodies[0])
+                                {
+                                    bOne = b1;
+                                }
+                                
+                            }
+                        }
+
                     }
                     
                     
@@ -182,6 +388,7 @@ public class main : MonoBehaviour
 
         newPlanetScript.rb.velocity = new Vector2(xv, yv);
         bodies.Add(newPlanet);
+        
     }
     void SpawnNewParticle(float x, float y, float xv, float yv, float m, bool isFocused)
     {
@@ -208,7 +415,15 @@ public class main : MonoBehaviour
     }
     public void AdjustNewMass(float NM)
     {
-        newMass = NM;
+        float nM = NM;
+        nM = Mathf.Round(nM * 10f) / 10f;
+        newMass = nM;
+    }
+    public void AdjustTime(float TS)
+    {
+        float ts = TS;
+        ts = Mathf.Round(ts * 10f) / 10f;
+        TimeStep = ts;
     }
     public void SliderHoverOver()
     {
@@ -244,5 +459,19 @@ public class main : MonoBehaviour
     {
         var v = Mathf.Sqrt((g * m) / r);
         return (v);
+    }
+    Vector2 GetMassCenter(Vector2 obj1Position, Vector2 obj2Position,float obj1Mass, float obj2Mass)
+    {
+        
+        float PointX = WeightedAverage(obj1Position.x, obj1Mass, obj2Position.x, obj2Mass);
+        float PointY = WeightedAverage(obj1Position.y, obj1Mass, obj2Position.y, obj2Mass);
+        Vector2 MassCenter = new Vector2(PointX, PointY);
+        return (MassCenter);
+    }
+    float CircularOrbitThing(float m1, float m2)
+    {
+        float q = m1 / m2;
+        float velMult = Mathf.Sqrt(q / 2)/m2;
+        return (velMult);
     }
 }
